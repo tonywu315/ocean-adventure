c17b3539af98f8481bfd1c2322b04f42f381414b
Tony Wu <tonywu315@gmail.com>
2022-04-10 01:48:15 -0400

    Move sea creatures

--- src/ocean.js
+++ src/ocean.js
@@ -6,66 +6,101 @@ import "./ocean.scss";
 const startColor = [25, 171, 222];
 
 export default class Ocean extends React.Component {
+    scrollRef = React.createRef();
+
+    state = {
+        scroll: 0,
+    };
+
+    componentDidMount() {
+        window.addEventListener("scroll", this.onScroll);
+    }
+
     getGradient = (percent) => {
         return (
-            "rgb(" + [startColor.map((color) => color * (1 - percent))] + ")"
+            "rgb(" +
+            [startColor.map((color) => color * Math.pow(1 - percent, 3))] +
+            ")"
         );
     };
 
+    onScroll = () => {
+        const winScroll =
+            document.body.scrollTop || document.documentElement.scrollTop;
+        const height =
+            document.documentElement.scrollHeight -
+            document.documentElement.clientHeight;
+        const scroll = winScroll / height;
+
+        this.setState({ scroll });
+    };
+
     render = () => {
-        console.log(this.getGradient(1));
-        let background = this.getGradient(1);
-        let lightOpacity = 1;
+        let backgroundColor = this.getGradient(this.state.scroll);
+        let lightOpacity = this.state.scroll;
 
         return (
             <div className="ocean">
-                <Transition animal="phytoplankton" x={50} y={300} size={150} />
-                <Transition animal="seaturtle" x={85} y={400} size={200} />
-                <Transition animal="dolphin" x={20} y={375} size={200} />
-		    <Transition animal="bluefin_tuna" x={40} y={500} size={200} />
-		    <Transition animal="school_of_fish" x={75} y={675} size={200} />
-                <Transition animal="tigershark" x={.5} y={750} size={200} />
-                <Transition animal="giant_squid" x={7} y={1500} size={200} />
-                <Transition animal="octopus" x={77} y={1800} size={225} />
-                <Transition animal="bristlemouth" x={50} y={2250} size={150} />
-                <Transition animal="sperm_whale" x={16} y={3000} size={400} />
-                <Transition animal="blobfish" x={83} y={4000} size={200} />
-                <Transition animal="jellyfish" x={16} y={5000} size={200} />
+                <Transition animal="phytoplankton" x={50} y={1100} size={150} />
+                <Transition animal="seaturtle" x={76} y={1250} size={200} />
+                <Transition animal="dolphin" x={20} y={1275} size={200} />
+                <Transition animal="bluefin_tuna" x={40} y={1400} size={200} />
+                <Transition
+                    animal="school_of_fish"
+                    x={65}
+                    y={1600}
+                    size={200}
+                />
+                <Transition animal="tigershark" x={0.5} y={1650} size={200} />
+                <Transition animal="octopus" x={50} y={2700} size={425} />
+                <Transition animal="bristlemouth" x={50} y={3050} size={150} />
+                <Transition animal="sperm_whale" x={16} y={3800} size={450} />
+                <Transition animal="giant_squid" x={7} y={4300} size={375} />
+                <Transition animal="blobfish" x={77} y={5000} size={200} />
+                <Transition animal="jellyfish" x={16} y={5800} size={800} />
                 <Transition
                     animal="red_velvet_whale_fish"
-                    x={66}
-                    y={6000}
+                    x={34}
+                    y={7800}
                     size={100}
                 />
                 <Transition
                     animal="humpback_angler_fish"
-                    x={50}
-                    y={8000}
-                    size={200}
+                    x={45}
+                    y={8800}
+                    size={250}
                 />
-			<Transition
+                <Transition
+                    animal="humpback_angler_fish"
+                    x={55}
+                    y={8850}
+                    size={40}
+                />
+                <Transition
                     animal="hydrothermal_vent"
                     x={5}
-                    y={10000}
+                    y={9497}
                     size={500}
                 />
                 <Transition
                     animal="vulcanoctopus_hydrothermalis"
                     x={10}
-                    y={10250}
+                    y={9547}
                     size={200}
                 />
-                
                 <Particles
                     id="tsparticles"
                     options={{
-                        background: { color: { value: background } },
+                        background: {
+                            color: { value: "#333333" },
+                            opacity: 0,
+                        },
                         particles: {
                             number: {
                                 density: { enable: true, area: 3200 },
                                 value: 800,
                             },
-                            color: { value: "#ffffff" },
+                            color: { value: "#ffffff", opacity: 0 },
                             shape: { type: "circle" },
                             size: {
                                 anim: { random: true },
@@ -103,6 +138,7 @@ export default class Ocean extends React.Component {
                         },
                     }}
                 />
+                <div id="background" style={{ backgroundColor }}></div>
             </div>
         );
     };
