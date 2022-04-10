bc03b5b204d47c0234eb28413d399c591b81b0cd
Tyler Yup <tyleryup1@gmail.com>
2022-04-10 00:28:08 -0400

    Preliminary Positions

--- src/ocean.js
+++ src/ocean.js
@@ -19,40 +19,43 @@ export default class Ocean extends React.Component {
 
         return (
             <div className="ocean">
-                <Transition animal="phytoplankton" x={10} y={100} size={200} />
-                <Transition animal="seaturtle" x={10} y={200} size={200} />
-                <Transition animal="dolphin" x={10} y={300} size={200} />
-                <Transition animal="tigershark" x={10} y={400} size={200} />
-                <Transition animal="giant_squid" x={10} y={500} size={200} />
-                <Transition animal="octopus" x={10} y={600} size={200} />
-                <Transition animal="bristlemouth" x={10} y={700} size={200} />
-                <Transition animal="sperm_whale" x={10} y={800} size={200} />
-                <Transition animal="blobfish" x={10} y={900} size={200} />
-                <Transition animal="jellyfish" x={10} y={1000} size={200} />
+                <Transition animal="phytoplankton" x={750} y={300} size={150} />
+                <Transition animal="seaturtle" x={1400} y={400} size={200} />
+                <Transition animal="dolphin" x={300} y={375} size={200} />
+		    <Transition animal="bluefin_tuna" x={650} y={500} size={200} />
+		    <Transition animal="school_of_fish" x={1100} y={675} size={200} />
+                <Transition animal="tigershark" x={10} y={750} size={200} />
+                <Transition animal="giant_squid" x={100} y={1500} size={200} />
+                <Transition animal="octopus" x={1250} y={1800} size={225} />
+                <Transition animal="bristlemouth" x={750} y={2250} size={150} />
+                <Transition animal="sperm_whale" x={250} y={3000} size={400} />
+                <Transition animal="blobfish" x={1250} y={4000} size={200} />
+                <Transition animal="jellyfish" x={250} y={5000} size={200} />
                 <Transition
                     animal="red_velvet_whale_fish"
-                    x={10}
-                    y={1100}
-                    size={200}
+                    x={1000}
+                    y={6000}
+                    size={100}
                 />
                 <Transition
                     animal="humpback_angler_fish"
-                    x={10}
-                    y={1200}
+                    x={750}
+                    y={8000}
                     size={200}
+                />
+			<Transition
+                    animal="hydrothermal_vent"
+                    x={70}
+                    y={10000}
+                    size={500}
                 />
                 <Transition
                     animal="vulcanoctopus_hydrothermalis"
-                    x={10}
-                    y={1300}
+                    x={200}
+                    y={10250}
                     size={200}
                 />
-                <Transition
-                    animal="hydrothermal_vent"
-                    x={10}
-                    y={1400}
-                    size={200}
-                />
+                
                 <Particles
                     id="tsparticles"
                     options={{
