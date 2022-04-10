f7b9d9ca29e0d6852e515fc1d17dc36b7e867be2
Tyler Yup <tyleryup1@gmail.com>
2022-04-10 03:10:18 -0400

    text test

--- src/data.js
+++ src/data.js
@@ -2,5 +2,12 @@ export const data = {
     "png name": {
         "name": "nameofcreature",
         "caption": "restoftext\nmoretext\n"
+    },
+	"bluefin_tuna": {
+        "name": "Atlantic Bluefin Tuna",
+        "caption": "Color: Dark blue, gray, and gold\nSize:  6-8 ft adult length\nHabitat: Near to the surface but may dive up to 1000m
+\nPrey: Omnivorous - Plankton, smaller fish, and crustaceans\nBehavior: Usually solitary, can be found in small groups of up to six; nocturnal, feeds at night inshore
+\nFun fact: They need to be constantly moving to survive"
     }
+
 }
